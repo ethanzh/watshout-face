aee42634fbdc39488fff0c4d6acb464df8851db1
Ethan Houston <ethan.houston@gmail.com>
2018-04-08 16:26:09 -0700

    Pushing to firebase via REST works

--- app/src/main/java/com/watshout/face/MainActivity.java
+++ app/src/main/java/com/watshout/face/MainActivity.java
@@ -1,8 +1,10 @@
 package com.watshout.face;
 
+import java.io.IOException;
+
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,31 +33,62 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Log.wtf("test", "test");
+                //makeGetRequest();
 
-                // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                String url = "https://personalsite-backend.firebaseio.com/users.json";
-
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                text.setText("Response is: " + response);
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        text.setText("That didn't work!");
-                    }
-                });
-
-                queue.add(stringRequest);
+                new MyDownloadTask().execute();
 
             }
         });
 
     }
+
+    void makeGetRequest() {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://personalsite-backend.firebaseio.com/users.json";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        text.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                text.setText("That didn't work!");
+            }
+        });
+
+        queue.add(stringRequest);
+
+    }
+}
+
+class MyDownloadTask extends AsyncTask<Void,Void,Void> {
+    protected void onPreExecute() {
+        //display progress dialog.
+
+    }
+    protected Void doInBackground(Void... params) {
+        PostExample example = new PostExample();
+        String json = example.bowlingJson("Jesse", "Jake");
+        String response = null;
+        try {
+            response = example.post("https://personalsite-backend.firebaseio.com/users.json", json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response);
+
+        return null;
+    }
+
+    protected void onPostExecute(Void result){
+        //
+    }
+
 }
---
+++ app/src/main/java/com/watshout/face/PostExample.java
@@ -0,0 +1,46 @@
+package com.watshout.face;
+
+import android.os.NetworkOnMainThreadException;
+import android.util.Log;
+
+import java.io.IOException;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class PostExample {
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (NetworkOnMainThreadException e) {
+            Log.e("error", e.toString());
+            return "null";
+        }
+
+    }
+
+    String bowlingJson(String player1, String player2) {
+        return "{\"9999\": {\"number\": \"12345\", \"username\":\"ethan\"}}";
+    }
+
+    public static void main(String[] args) throws IOException {
+        PostExample example = new PostExample();
+        String json = example.bowlingJson("Jesse", "Jake");
+        String response = example.post("http://www.roundsapp.com/post", json);
+        System.out.println(response);
+    }
+}
