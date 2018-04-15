13eec21685628fd01ee9fee05ede1596944e3131
Ethan Houston <ethan.houston@gmail.com>
2018-04-15 02:39:34 -0700

    uploading GPS data works

--- app/src/main/java/com/watshout/face/MainActivity.java
+++ app/src/main/java/com/watshout/face/MainActivity.java
@@ -1,51 +1,33 @@
 package com.watshout.face;
 
-import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
+import android.provider.Settings.Secure;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
-import android.provider.Settings.Secure;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.akhgupta.easylocation.EasyLocationAppCompatActivity;
+import com.akhgupta.easylocation.EasyLocationRequest;
+import com.akhgupta.easylocation.EasyLocationRequestBuilder;
+
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
-import android.util.Log;
-import android.view.Menu;
+import java.io.IOException;
+import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLocation;
-    private LocationManager locationManager;
-    private LocationRequest mLocationRequest;
-
-    private boolean hasGps() {
-        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
-    }
+public class MainActivity extends EasyLocationAppCompatActivity {
 
     @SuppressLint("HardwareIds")
     private String getID() {
@@ -60,13 +42,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         TextView text = findViewById(R.id.text);
         Button button = findViewById(R.id.button);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        @SuppressLint("RestrictedApi")
+        LocationRequest locationRequest = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setInterval(5000)
+                .setFastestInterval(5000);
+
+        EasyLocationRequest easyLocationRequest = new EasyLocationRequestBuilder()
+                .setLocationRequest(locationRequest)
+                .setFallBackToLastLocationTime(3000)
+                .build();
+
+        requestLocationUpdates(easyLocationRequest);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,81 +74,76 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
     @Override
-    public void onConnected(Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        } startLocationUpdates();
-        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(mLocation == null){
-            startLocationUpdates();
+    public void onLocationPermissionGranted() {
+        Log.wtf("GPS", "Permission Granted");
+    }
+
+    @Override
+    public void onLocationPermissionDenied() {
+        Log.wtf("GPS", "Permission Denied");
+    }
+
+    @Override
+    public void onLocationReceived(Location location) {
+
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+
+        String toPost = "{\"lat\": " + lat + ", \"long\": " + lon + "}";
+
+        Log.wtf("GPS", toPost);
+
+        PostData post = new PostData();
+
+        post.execute(toPost);
+
+    }
+
+    @Override
+    public void onLocationProviderEnabled() {
+        Log.wtf("GPS", "Enabled");
+    }
+
+    @Override
+    public void onLocationProviderDisabled() {
+        Log.wtf("GPS", "Disabled");
+    }
+
+}
+
+class PostData extends AsyncTask<String, Void, Void> {
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient client = new OkHttpClient();
+
+    protected void onPreExecute() {
+        //display progress dialog.
+
+    }
+
+    protected Void doInBackground(String... strings) {
+
+        String jsonData = strings[0];
+
+        RequestBody body = RequestBody.create(JSON, jsonData);
+        Request request = new Request.Builder()
+                .url("https://personalsite-backend.firebaseio.com/coords.json")
+                .post(body)  // Changing this from put to post changes behavior
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if (mLocation != null) {
-            double latitude = mLocation.getLatitude();
-            double longitude = mLocation.getLongitude();
 
-            Toast.makeText(this, Double.toString(latitude), Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Location not Detected", Toast.LENGTH_SHORT).show();
-        }
+        return null;
     }
 
-    protected void startLocationUpdates() {
-        // Create the location request
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(50)
-                .setFastestInterval(10);
-        // Request location updates
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
-                mLocationRequest, this);
-        Log.d("reque", "--->>>>");
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i("GPS", "Connection Suspended");
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i("GPS", "Connection failed. Error: " + connectionResult.getErrorCode());
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-    @Override
-    public void onLocationChanged(Location location) {
+    protected void onPostExecute(Void result) {
 
     }
+
 }
 
 class PutData extends AsyncTask<Void, Void, Void> {
@@ -193,3 +176,4 @@ class PutData extends AsyncTask<Void, Void, Void> {
     }
 
 }
+
